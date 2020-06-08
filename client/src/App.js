7735623bff73991117164dcd59003df9befc528a
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-08 02:42:51 -0700

    setup redux store, alertReducer

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,23 @@ import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./App.css";
 
 const App = () => (
-  <BrowserRouter>
-    <Navbar />
-    <Route exact path="/" component={Landing} />
-    <section className="container">
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </section>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
